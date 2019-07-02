5d2733ca16dce9062ed6d9cf28eee1b4b9d1fcc9
Raul Gonzalez <rgonz166@gmail.com>
2019-07-01 20:41:13 -0700

    now updates win and loss variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,29 @@
 // Declare variables
-
     //List of League of Legends champions from 2012
     var championNames = ["Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Ashe", "Blitzcrank", "Brand", "Caitlyn", "Cassiopeia", "Cho'gath", "Corki", "Dr. Mundo", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gragas", "Graves", "Hecarim", "Heimerdinger", "Irelia", "Janna", "Jarvan IV", "Jax", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kennen", "Kog'Maw", "LeBlanc", "Lee Sin", "Leona", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "Master Yi", "Miss Fortune", "Mordekaiser", "Morgana", "Nasus", "Nautilus", "Nidalee", "Nocturne", "Nunu", "Olaf", "Orianna", "Pantheon", "Poppy", "Rammus", "Renekton", "Riven", "Rumble", "Ryze", "Sejuani", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Talon", "Taric", "Teemo", "Tristana", "Trundle", "Trydamere", "Twisted Fate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xerath", "Xin Zhao", "Yorick", "Ziggs", "Zilean"]
 
-    // For testing space, . and '
-    // var championNames = ["Miss Fortune", "Jarvan IV", "Dr. Mundo","Kog'Maw"]
+    
 
     var chosenName = "";
     var chosenNameLower = "";
+    var userGuess = "";
     var answerArray = [];
     var guesses = [];
-    var userGuess = "";
     var health = 10;
+    var wins = 0;
+    var losses = 0;
     var hasLetter = false;
+
+    // Get Elements
     var setBlank = document.getElementById("blankName");
     var lettersUsed = document.getElementById("letters-used");
     var healthText = document.getElementById("health-text");
+    var winsText = document.getElementById("wins-text");
+    var lossesText = document.getElementById("losses-text");
+
     
     // Get random name from array
-    var chosenName = championNames[Math.floor(Math.random()*championNames.length)];
+    chosenName = championNames[Math.floor(Math.random()*championNames.length)];
     chosenNameLower = chosenName.toLowerCase();
     console.log(chosenName);
     
@@ -75,12 +80,19 @@
         }
         else{
             setBlank.innerText = answerArray.join('');
+            if(answerArray === chosenName){
+                wins++;
+                alert("Congratulations! You won. Play another?");
+                winsText.innerText = wins;
+            }
         }
     }
     function checkHealth(){
         if(health <= 0){
             revealAnswer();
-            alert("Game Over! You lost")
+            losses++;
+            alert("Game Over! You lost");
+            lossesText.innerText = losses;
         }
     }
 
@@ -93,16 +105,8 @@
 
     // print array
     /*TODO 
-        1.add guessed letters array for user so they dont repeat letters
-        2.remaining guesses
-        3. start game, wins losses, has finished boolean
-        4. store guessed in var and check against name array using for loop, if matches, then replace that index with guess
-        for(var i=0; i < name.lenght; i++){
-            if(name[i] === guess){
-                answerArray[i] = guess;
-                remainingLetters--;
-            }
-        }
-        REMINDER: use chosen name to replace the displayed array correct answer to correct for capitalization in the beginning of names
-            *also use toLower() to check if letter is the same as chosen name despite upper or lower case.
+        * start game, wins losses, has finished boolean
+        * store guessed in var and check against name array using for loop, if matches, then replace that index with guess
+        * integrate letters for mobile under certain screen size
+        * 
     */
