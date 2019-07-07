e5afb70913086934bc186de045286fdcd03f1255
Raul Gonzalez <rgonz166@gmail.com>
2019-07-07 01:43:59 -0700

    fixed user input so only letters can be pressed and other characted will alert user to enter only letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 // Declare variables
     //List of League of Legends champions from 2012
     var championNames = ["Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Ashe", "Blitzcrank", "Brand", "Caitlyn", "Cassiopeia", "Cho'gath", "Corki", "Dr. Mundo", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gragas", "Graves", "Hecarim", "Heimerdinger", "Irelia", "Janna", "Jarvan IV", "Jax", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kennen", "Kog'Maw", "LeBlanc", "Lee Sin", "Leona", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "Master Yi", "Miss Fortune", "Mordekaiser", "Morgana", "Nasus", "Nautilus", "Nidalee", "Nocturne", "Nunu", "Olaf", "Orianna", "Pantheon", "Poppy", "Rammus", "Renekton", "Riven", "Rumble", "Ryze", "Sejuani", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Talon", "Taric", "Teemo", "Tristana", "Trundle", "Trydamere", "Twisted Fate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xerath", "Xin Zhao", "Yorick", "Ziggs", "Zilean"]
+    var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
     // var championNames = ["Dr. Mundo","Twisted Fate"];
     
@@ -15,6 +16,7 @@
     var wins = 0;
     var losses = 0;
     var hasLetter = false;
+    var isLetter = false;
 
     // Get Elements
     var setBlank = document.getElementById("blankName");
@@ -36,11 +38,11 @@ window.onload = function(){
         healthBar.style.width = (health*10) +'%';
         healthBar.innerText = (health*10) + '%';
         healthBar.className = "progress-bar center-block bg-success";
+        isLetter = false;
         
         // Get random name from array
     chosenName = championNames[Math.floor(Math.random()*championNames.length)];
 
-    console.log(chosenName);
     nameCheck = chosenName.replace(' ','-');
     
     // Get empty spaces for random name
@@ -63,10 +65,23 @@ window.onload = function(){
     lettersUsed.innerText = guesses.join("");
     }
 
+    // When Key is Pressed run this
     document.onkeyup = function(event) {
-        userGuess = event.key;
-        checkGuesses(userGuess.toLowerCase());
+        userGuess = event.key.toLocaleLowerCase();
+        isLetter = false;
+        for(var i=0; i < letters.length;i++){
+            if (userGuess === letters[i]){
+                isLetter = true;
+            }
+        }
+        if(isLetter){
+            checkGuesses(userGuess);
+        }
+        else{
+            alert("Please only enter a letter!");
+        }
     }
+    // TODO: ADD BUTTON FUNCTIONALITY FOR MOBILE
 
     // check if letter has been used
     function checkGuesses(guess){
@@ -102,7 +117,10 @@ window.onload = function(){
             healthText.textContent = health;
             healthBar.style.width = (health*10) +'%';
             healthBar.innerText = (health*10) + '%';
-            if(health > 3 && health <=5){
+            if(health >5){
+                healthBar.className = "progress-bar center-block bg-success";
+            }
+            else if(health > 3 && health <=5){
                 healthBar.className = "progress-bar center-block bg-warning";
             }
             else if(health <= 3){
